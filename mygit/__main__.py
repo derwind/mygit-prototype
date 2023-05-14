350b06338289a32fcde38c30993f9c9cc13a9c6e
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-05-15 01:02:06 +0900

    stub implementation of 'init' and 'add'

---
+++ mygit/__main__.py
@@ -0,0 +1,33 @@
+import argparse
+from .mygit import init, add
+
+
+def command_init(args):
+    init()
+
+
+def command_add(args):
+    add()
+
+
+def main():
+    '''Entrypoint'''
+
+    parser = argparse.ArgumentParser(prog='mygit-prototype', description='A prototype of an imitation of Git')
+    subparsers = parser.add_subparsers()
+
+    subparser = subparsers.add_parser('init')
+    subparser.set_defaults(handler=command_init)
+
+    subparser = subparsers.add_parser('add')
+    subparser.set_defaults(handler=command_add)
+
+    args = parser.parse_args()
+    if hasattr(args, 'handler'):
+        args.handler(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
